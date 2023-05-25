83bec20823085af649bddf67b6d47e05b343465c
burhan <alacaburhan122016@gmail.com>
2023-05-25 17:41:00 +0300

    loops

--- Loops(Döngüler)/loops.js
+++ Loops(Döngüler)/loops.js
@@ -1,3 +1,56 @@
+// // //!1 den 10 a kadar yazdıralım
+
+//  for(let i=1; i<=20;i++){
+//      if(i<=20){
+//          console.log(i)
+//      }
+//  }
+
+// // //! 1 den 10 a  kadar çift sayıları yazalım
+//  for(let i=0;i<=10;i=i+2){
+//      console.log(i)
+//  }
+
+// //!bir dizideki sayılarıntoplamını hesaplayan uygulamayı for döngüsü ile yazınız
+// const sayı =[1,2,3,4,5,6,7];
+// let sum=0;
+
+// for(let i=0; i<sayı.length; i++ ){
+// sum+=sayı[i];
+// console.log(sum)
+// }
+// console.log(sum)
+// //!------------- ÖRNEK -------------------
+//? grades'in ortalamasini hesaplayiniz.
+//  const grades = [55, 77, 23, 89, 100, 44, 33, 45]
+
+//  let sum = 0;
+//  for(let i=0; i<grades.length;   i++){
+
+//     sum+=grades[i]
+//     console.log(sum)
+//  }
+//  console.log("SUM:",sum)
+//  console.log("AVG:",sum/grades.length)
+
+
+ //!grades dizisindeki notların 50'den kucuk olanları ve buyuk olanları 2 ayri diziye kaydediniz.
+const grades = [55, 77, 23, 89, 100, 44, 33, 45]
+const lessThan50=[];
+const equalOrBiggerThan50=[];
+
+for(let j=0; j<grades.length;  j++){
+if(grades[j]<50){
+    lessThan50.push(grades[j])
+}else{
+    equalOrBiggerThan50.push(grades[j])
+}
+}
+console.log(lessThan50)
+console.log(equalOrBiggerThan50)
+
+
+
 
 // soru-1 1000 ile 2000 arasındaki 13 e bölümünden kalanı 3 olan sayıları konsola yazdırınız ?
 
