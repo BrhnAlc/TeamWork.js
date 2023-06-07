c8f063da6db5c0df33278cd8d361fee5818046ef
burhan <alacaburhan122016@gmail.com>
2023-06-07 16:31:36 +0300

    tw-6

--- DOM/EVENTS(OLAYLAR)/events.js
+++ DOM/EVENTS(OLAYLAR)/events.js
@@ -1,3 +1,30 @@
 
+//!JavaScript'te "events" (olaylar), web sayfalarında gerçekleşen kullanıcı etkileşimlerini veya tarayıcıdaki diğer olayları yakalamak ve işlemek için kullanılan özelliklerdir. İşte bazı yaygın kullanılan olaylar ve onlara ilişkin örnekler:
 
+//  ****1-onClick (Tıklama Olayı):Bu olay, bir elemente tıklandığında tetiklenir.Örnek:
+
+document.getElementById("myButton").addEventListener("click", function() {
+    console.log("Butona tıklandı!");
+  })
+  //***** 2-onKeyUp (Tuş Bırakma Olayı):Bu olay, bir tuşun klavyeden bırakıldığında tetiklenir.Örnek:
+
+
+document.getElementById("myInput").addEventListener("keyup", function(event) {
+  console.log("Tuş bırakıldı. Basılan tuş: " + event.key);
+});
+//***3-onMouseOver (Üzerine Gelme Olayı):Bu olay, bir elementin üzerine gelindiğinde tetiklenir.Örnek:
+
+document.getElementById("myElement").addEventListener("mouseover", function() {
+  console.log("Elementin üzerine gelindi!");
+});
+
+//*4-onSubmit (Form Gönderme Olayı):Bu olay, bir formun gönderildiğinde tetiklenir.Örnek:
+
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Formun otomatik olarak gönderilmesini engeller
+  console.log("Form gönderildi!");
+});
+
+//*Bu örneklerde addEventListener yöntemi kullanılarak belirli bir olaya dinleyici (event listener) ekleniyor. Dinleyici, belirli bir olay gerçekleştiğinde tetiklenen bir işlevdir. İşlev, olayın nasıl işleneceğini tanımlar. Bu örneklerde, olay gerçekleştiğinde konsol mesajı yazdırmak gibi basit işlemler gerçekleştiriliyor.
 
---
+++ tw-6/javascript.js
@@ -0,0 +1,128 @@
+Ask Questions 15m
+1. Sass is a _____.
+A. Scripting language 
+B. Markup language
+C. CSS pre-processor
+D. Programming Language
+2. Sass stands for ______.
+A. Semantically Awesome Stylesheet
+D. Syntactically Awesome Stylesheet 
+C. Simple Awesome Stylesheet 
+D. Syntax-based Awesome Stylesheet
+3. What are the benefits of using SASS?
+A. It is a pre-processing language which provides its own syntax for CSSwrong
+B. It is a superset of CSS which contains all the features of CSS and is an open source pre-processor, coded in
+Ruby
+C. It is more stable and powerful CSS extension and style documents more clearly and structurally
+D. All of the above
+tw-006.md 6/7/2023
+3 / 7
+4. In which year was SASS introduced?
+A. 2005 
+B. 2006 
+C. 2008
+D. 2009
+5. Which of the following directive displays the SassScript expression value as fatal error?
+A. @error
+B. @warn
+C. @at-root
+D. None of the above
+6. SASS was created by...
+A. Linus Torvalds 
+B. Brendan Eich 
+C. Hampton Catlin
+D. Guido van Rossum
+7. In Sass, which of the following is the correct way to define a variable?
+A. #primary-color: #888;
+B. @primary-color: #888;
+C. %primary-color: #888;
+D. $primary-color: #888;
+8. Which is the correct syntax to declare a variable "myfonts" assigning the two font names?
+A. $myfonts: Helvetica, and sans-serif;
+B. $myfonts: Helvetica, sans-serif;
+C. $myfonts: "Helvetica, sans-serif";
+D. $myfonts: "Helvetica+sans-serif";
+9. Which directive is used to create CSS code that is to be reused throughout the website?
+A. @import
+B. @define
+C. @mixin
+D. All of the above
+tw-006.md 6/7/2023
+4 / 7
+10. Which directive is used to share a set of CSS properties from one selector to another?
+A. @share 
+B. @import 
+C. @transfer
+D. @extend
+11. What is the output of this code block?
+let daltones = ['joe', 'Jack', 'Willam', 'Averell']
+daltones.shift()
+daltones.pop()
+console.log(daltones)
+A. ['joe', Jack']
+B. []
+C. ['Jack', 'Willam']
+D. ['joe','Averell']
+
+
+12. What is the output of this code block?
+let daltones = ['joe', 'Jack', 'Willam', 'Averell']
+for(let i =0; i< daltones.length; i++){
+ if(i == 1){
+ continue;
+ }
+ console.log(daltones[i]);
+}
+A. Jack
+B. joe, Jack
+C. joe, Jack, Willam
+D. joe, Willam, Averell
+13. What is the new avengers array after this code block?
+let avengers = ['Iron Man', 'Captain America', 'Black Widow', 'Hulk']
+avengers.splice(2,1,'Thor', 'Hawkeye');
+tw-006.md 6/7/2023
+5 / 7
+A. ['Black Widow'] 
+B. index error
+C. ['Iron Man', 'Captain America', 'Thor', 'Hawkeye', 'Hulk']
+D. ['Iron Man', 'Captain America', 'Thor', 'Hawkeye','Black Widow', 'Hulk']
+14. Write a for loop for iterate languages?
+let fullStack = {
+ languages: ["JavaScript", "React", "HTML"],
+ jira: true,
+ gitHub: true,
+ difficulty: 8,
+}
+for(){
+//write your code here
+}
+//output : JavaScript, React, HTML
+15. Write a code for get fullStack object's keys
+let fullStack = {
+ languages: ["JavaScript", "React", "HTML"],
+ jira: true,
+ gitHub: true,
+ difficulty: 8,
+}
+//write your code here
+//output : languages, jira, gitHub, difficulty
+tw-006.md 6/7/2023
+6 / 7
+16. Write a method to get myCar's age
+const myCar = {
+ make : 'ford',
+ model : 'Mustang',
+ year : 1965,
+ color : 'Black'
+ }
+ /* Write your code here 
+ myCar.age = 
+ */
+myCar.age(2023) //Output: 58
+Interview Questions 15m
+1. What Are The Number Methods in javaScript?
+2. What Is Javascript Date Object?
+3. What Are Date Methods?
+4. Explain how to define a variable in Sass?
+5. Explain what is the difference between Sass and SCSS?
+
