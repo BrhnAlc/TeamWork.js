b5a7598be404772b564d86ca57ddfc5310dff57b
burhan <alacaburhan122016@gmail.com>
2023-06-14 23:25:34 +0300

    senkron

--- Asenkron/app.js
+++ Asenkron/app.js
@@ -17,6 +17,9 @@
 //? Dosya Okuma/Yazma islemleri gibi zaman tuketen kodlarda Asyn Programlama
 //? kullanilmasi cok onemlidir.
 
-//* Senkron
+//* "Senkron": terimi, programın işlemleri sırayla ve eşzamanlı bir şekilde gerçekleştirdiği bir programlama yaklaşımını ifade eder. Senkron programlama, bir işlem tamamlanmadan diğer işlemlere geçmez ve işlemler birbirini bekleyebilir.
 //* ------------------------------------------------
 
+//*"Asenkron" terimi, programın eşzamanlı olmayan veya zaman uyumsuz bir şekilde çalışmasını ifade eder. Asenkron programlama, belirli bir işlem tamamlanmadan diğer işlemlere geçme yeteneği sağlar. Bu, uzun süren işlemlerin veya beklemelerin diğer işlemleri etkilemeden gerçekleştirilmesini sağlar.
+
+//*JavaScript, asenkron programlamayı destekleyen bir dildir. İşte JavaScript'te asenkron programlamanın basit örneklerinden üç tanesi:
