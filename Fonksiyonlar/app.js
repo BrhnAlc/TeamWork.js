af2b617bfbef4f2606d9e58bbc6feff1987a4668
burhan <alacaburhan122016@gmail.com>
2023-06-16 00:14:34 +0300

    btn

---
+++ Fonksiyonlar/app.js
@@ -0,0 +1,13 @@
+function selamlar(mesaj){
+    console.log("merhaba");
+}
+
+selamlar();
+console.log("mesaj");
+
+
+function yasHesapla(dogumYılı){
+    console.log( new Date().getFullYear() - dogumYılı);
+}
+
+yasHesapla(1988);
