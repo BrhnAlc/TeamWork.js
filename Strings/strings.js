60dbca921c24ce37fa32ff295c04d2215c955042
burhan <alacaburhan122016@gmail.com>
2023-06-17 23:23:43 +0300

    toUppercase

--- Strings/strings.js
+++ Strings/strings.js
@@ -14,3 +14,12 @@ let rakam1="1,2,3,4,5,6,7,";
 let karekterSayısı1=rakam1[3];
 
 console.log(karekterSayısı1);
+
+
+//!Soru 3: Verilen bir dizeyi nasıl büyük harflere dönüştürebilirsiniz?
+
+const meyve="armut,elma,domates";
+
+const buyukHarf=meyve.toUpperCase();
+
+console.log(buyukHarf);
