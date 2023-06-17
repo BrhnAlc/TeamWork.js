804877282af6729f395136c4a3a73ecaba5ff442
burhan <alacaburhan122016@gmail.com>
2023-06-18 02:15:25 +0300

    return

--- Fonksiyonlar/app.js
+++ Fonksiyonlar/app.js
@@ -16,3 +16,27 @@ function square(){
 square();
 
 //? parametresiz fonksiyon
+
+function addTwoNumbers(){
+    let numOne=10;
+    let numTwo=20;
+    let sum=numOne+numTwo;
+    console.log(sum);
+
+}
+addTwoNumbers();
+//*bir işlevin çalıştırılabilmesi için adıyla çağrılması gerekir
+
+
+function printFullName(){
+    let firstName="burhan";
+    let lastName="alaca"
+    let space=" ";
+    let fullName=firstName+space+lastName;
+    console.log(fullName);
+}
+printFullName();
+
+
+//?Fonksiyon dönen değer
+//*İşlev ayrıca değerler de döndürebilir, eğer bir işlev değer döndürmezse, işlevin değeri tanımsızdır. Yukarıdaki fonksiyonları return ile yazalım. Bundan sonra, bir işleve yazdırmak yerine değer döndürüyoruz.
