0d86dfe20cf88663e70ef7a350acd8fb170ccfe9
burhan <alacaburhan122016@gmail.com>
2023-06-17 23:17:49 +0300

    karekter alma

--- Strings/strings.js
+++ Strings/strings.js
@@ -6,3 +6,11 @@ let rakam="1,2,3,4,5,6,7,";
 let karekterSayısı=rakam.length;
 
 console.log(karekterSayısı);
+
+//!Soru 2:Bir dizenin belirli bir indeksteki karakterini nasıl alabilirsiniz?
+
+let rakam1="1,2,3,4,5,6,7,";
+
+let karekterSayısı1=rakam1[3];
+
+console.log(karekterSayısı1);
