c309aea2d9dc989a57dc71b39e81b265292185b4
burhan <alacaburhan122016@gmail.com>
2023-06-18 18:09:05 +0300

    innerText

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,21 +1,16 @@
 
+let sonuc;
+sonuc=document.getElementById("title");
+sonuc=document.getElementById("title").id;
+sonuc=document.getElementById("title").classList;
+sonuc=document.getElementById("title").style.fontSize="30px";
+sonuc=document.getElementById("title").style.color="red";
+
+
+sonuc=document.getElementById("title").innerText="Burhan alaca";
+sonuc=document.getElementById("title").innerHTML="<p>alaca</p>";
 
 
 
-const ekleBtn=document.querySelector(`#btnAddNewTask`);
 
-
-
-
-ekleBtn.addEventListener(`click`,function(){
-    console.log("burhan");
-})
-
-const temizleBtn=document.querySelector(`#btnClear`);
-
-
-
-temizleBtn.addEventListener(`click`,function(e){
-   
-    e.target.classList.add("btn-lg")
-})
+console.log(sonuc);
